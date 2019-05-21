07e3fd645129965cbe2a5067d52b03a64f7b21ab
Matteo Rossi <teo.red90@gmail.com>
2019-05-21 16:32:32 -0700

    Compatibility with Ignis 0.1

--- example.py
+++ example.py
@@ -39,19 +39,21 @@ qst = tomo.state_tomography_circuits(circuit, q_reg)
 job = qiskit.execute(qst, BasicAer.get_backend('qasm_simulator'), shots=SHOTS)
 print('Time taken:', time.time() - t)
 
+tomo_fitter = tomo.fitters.base_fitter.TomographyFitter(job.result(), qst)
 # Extract tomography data so that counts are indexed by measurement configuration
-tomo_counts = tomo.tomography_data(job.result(), qst)
+#tomo_counts = tomo.tomography_data(job.result(), qst)
 
 # Generate fitter data and reconstruct density matrix
-probs, basis_matrix, weights = tomo.fitter_data(tomo_counts)
-print('tomography results',tomo_counts)
+probs, basis_matrix, weights = tomo_fitter._fitter_data(True, 0.5)
+#print('tomography results', tomo_counts)
 
 #bayesian reconstruction of the density matrix and error on observable
 rho_fit, error_rho = bme_fit(probs, basis_matrix, SHOTS)
 
+print("BME", rho_fit)
 #maximum likelihood reconstruction of density matrix
-rho_mle = tomo.state_mle_fit(probs, basis_matrix, weights)
-
+rho_mle = tomo_fitter.fit()
 
 print("MLE:", rho_mle)
-print("BME", rho_fit)
+
+print("error", error_rho)
